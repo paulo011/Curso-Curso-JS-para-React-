f87c9b31d913fc7d55c87221e2a07b887bf4f202
paulo011 <pauloortega011@gmail.com>
2022-04-03 17:52:43 -0300

    Create Cardgame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+
+import "./style.css"
+
+function CardGame(){
+    return `
+    <article class="Card-Game">
+        <img src="src/components/img/alura-pixel 1.png">
+    </article>
+    `;
+}
+
+export default CardGame;
